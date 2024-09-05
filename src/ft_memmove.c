06052d7fae9e0d419bba16d0cf3538323ed9d56d
Raphaël Ferreira <raphaelferreira@Raphaels-MacBook-Air.local>
2024-09-05 18:55:38 +0200

    nouveau dosser ajout des ficher de la libft + bonnus

---
+++ src/ft_memmove.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphael <raphael@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 18:28:18 by raphael           #+#    #+#             */
+/*   Updated: 2024/08/01 19:10:18 by raphael          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+
+	if (!dst && !src)
+		return (NULL);
+	if (dst > src)
+	{
+		i = 0;
+		while (i < len)
+		{
+			((char *)dst)[i] = ((char *)src)[i];
+			i++;
+		}
+	}
+	else
+	{
+		while (len > 0)
+		{
+			len--;
+			((char *)dst)[len] = ((char *)src)[len];
+		}
+	}
+	return (dst);
+}
